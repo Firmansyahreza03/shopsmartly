080758386e14c768cf77d0c5f1cd56995348d509
Reza <touchbar@TouchBars-MacBook-Pro.local>
2023-03-20 00:14:00 +0700

    initialize

---
+++ src/test/java/com/rezalab/shopsmartly/ShopSmartlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezalab.shopsmartly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSmartlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
